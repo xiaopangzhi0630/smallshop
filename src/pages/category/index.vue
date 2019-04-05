dcaae82539039b8cd0252b7d8b095f0bfa7fe124
xiaopangzhi <445917591@qq.com>
2019-04-05 22:59:21 +0800

    左侧对应的右侧列表数据跟新

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -2,16 +2,42 @@
     <div>
         <!-- 搜索 -->
         <search-bar></search-bar>
+        <!-- 菜单和内容 -->
+        <div class="content">
+            <!-- 左侧 -->
+            <div class="left">
+                <div :class="{active:currentIndex === index}" :key="item.cat_id" v-for="(item, index) in cate" class="menu-item">
+                    {{item.cat_name}}
+                </div>
+            </div>
+            <!-- 右侧 -->
+            <div class="right">
+                <div :key='item1.cat_id' v-for='item1 in rightData' class="brand-item">
+                    <div class="brand-title">
+                       {{item1.cat_name}}
+                    </div>
+                    <div class="brand-list">
+                        <div :key='i' v-for='(img, i) in item1.children' class="brand">
+                            <img :src="img.cat_icon" mode="aspectFill">
+                            <p>{{img.cat_name}}</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 import SearchBar from '../../components/searchbar'
 import request from '../../utils/request'
+
 export default {
   data () {
     return {
-      cate: []
+      cate: [],
+      currentIndex: 0,
+      rightData: []
     }
   },
   components: {
@@ -22,15 +48,18 @@ export default {
       // 调用接口获取数据
       let ret = await request('categories')
       this.cate = ret.data.message
-      console.log(ret)
+      // console.log(ret)
     }
   },
-  mounted () {
-    this.cateData()
+  async mounted () {
+    await this.cateData()
+    // 从当前分类数据中跟新出当前索引对应的右侧数据
+    this.rightData = this.cate[this.currentIndex].children
+    console.log(this.rightData)
   }
 }
 </script>
 
-<style>
-
+<style scoped lang='scss'>
+  @import 'main.scss'
 </style>
