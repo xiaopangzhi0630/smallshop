ecd6d16e625001575ebd68016b5a891b8bdefaaa
xiaopangzhi <445917591@qq.com>
2019-04-05 22:12:30 +0800

    获取分类左侧数据

--- src/pages/category/index.vue
+++ src/pages/category/index.vue
@@ -7,12 +7,26 @@
 
 <script>
 import SearchBar from '../../components/searchbar'
+import request from '../../utils/request'
 export default {
   data () {
-
+    return {
+      cate: []
+    }
   },
   components: {
     'search-bar': SearchBar
+  },
+  methods: {
+    async cateData () {
+      // 调用接口获取数据
+      let ret = await request('categories')
+      this.cate = ret.data.message
+      console.log(ret)
+    }
+  },
+  mounted () {
+    this.cateData()
   }
 }
 </script>
