340596786aa8f3a8364f8ac6c6be2a2f6c8c2747
xiaopangzhi <445917591@qq.com>
2019-04-05 21:22:39 +0800

    tab切换

---
+++ src/components/searchBar.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class='serach-bar'>
+      <div class='search-input'>
+        <icon type='search' color='#999' />
+        <span>搜索</span> 
+      </div>
+    </div>    
+</template>
+<style <style scoped>
+ /* 搜索 */
+.serach-bar {
+   background-color: #eb4450;
+   padding: 20rpx;
+ }
+ .search-input {
+   background-color: #fff;
+   text-align: center;
+   border-radius: 15rpx;
+ }
+ input {
+   padding: 10rpx;
+ }
+</style>
+
