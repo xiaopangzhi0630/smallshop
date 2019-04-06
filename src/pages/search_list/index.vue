ca30d38ac79b135bd270a1e0a6ab818464ffce13
xiaopangzhi <445917591@qq.com>
2019-04-06 21:28:20 +0800

    tab栏完成

--- src/pages/search_list/index.vue
+++ src/pages/search_list/index.vue
@@ -1,8 +1,21 @@
 <template>
     <div>
       <!-- 搜索条 -->
-      <search-bar></search-bar>
-    </div>
+      <!-- <search-bar></search-bar> -->
+      <div class="search">
+        <div class="search-input">
+          <icon type="search" size="16" color="#999"/>
+          {{keyword}}
+        </div>
+      </div>
+      <!-- 选项卡 -->
+      <div class="tabs">
+        <div @click="tabHandle(index)" :class="{active:currentIndex === index}" :key="index" v-for="(item, index) in tabNames" class="tab-item">
+            {{item}}
+        </div>
+      </div>
+
+    </div> 
 </template>
 <script>
 import SearchBar from '../../components/searchbar'
@@ -10,16 +23,31 @@ import SearchBar from '../../components/searchbar'
 export default {
   data () {
     return {
+      tabNames: ['综合', '销量', '价格'],
+      keyword: '',
+      currentIndex: 0
+    }
+  },
+  methods: {
+    tabHandle (index) {
+      // 改变当前Tab的索引
+      this.currentIndex = index
     }
   },
   components: {
     'search-bar': SearchBar
+  },
+  onLoad (params) {
+    // 小程序的生命周期
+    // 参数params表示路径传递过来的参数    传的是一个对象
+    // console.log(params)
+    this.keyword = params.query
   }
 }
 </script>
 
-<style scoped lang='scss>
-
+<style scoped lang='scss'>
+    @import 'main.scss'
 </style>
 
 
